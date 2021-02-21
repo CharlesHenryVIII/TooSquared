b951f5c180b67db02ff880ca92e71de422d3c314
CharlesHenryVIII <Tonykart84@gmail.com>
2021-02-21 10:36:21 -0700

    Moved ShaderProgram into Rendering

--- Source/Rendering.h
+++ Source/Rendering.h
@@ -4,6 +4,9 @@
 #include "Math.h"
 #include "Misc.h"
 
+#include <string>
+#include <Windows.h>
+
 enum class ts_MessageBox {
 	Invalid,
 	Error = SDL_MESSAGEBOX_ERROR,
@@ -38,5 +41,28 @@ public:
 	Texture(const char* fileLocation);
 	inline void Bind();
 };
+
+class ShaderProgram
+{
+    GLuint m_handle = 0;
+    std::string m_vertexFile;
+    std::string m_pixelFile;
+	FILETIME m_vertexLastWriteTime = {};
+	FILETIME m_pixelLastWriteTime = {};
+
+    ShaderProgram(const ShaderProgram& rhs) = delete;
+    ShaderProgram& operator=(const ShaderProgram& rhs) = delete;
+
+    bool CompileShader(GLuint handle, const char* text);
+
+public:
+    ShaderProgram(const std::string& vertexFileLocation, const std::string& pixelFileLocation);
+    ~ShaderProgram();
+    FILETIME GetShaderTimeAndText(uint8* buffer, const size_t bufferLength, const std::string& fileLoc);
+    void CheckForUpdate();
+    void UseShader();
+    void UpdateUniformMat4(const char* name, GLsizei count, GLboolean transpose, const GLfloat* value);
+};
+
 
 int32 CreateMessageWindow(SDL_MessageBoxButtonData* buttons, int32 numOfButtons, ts_MessageBox type, const char* title, const char* message);
